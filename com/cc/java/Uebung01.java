d285e67d2011c7807f8309712d364a11fdc32577
KrafO <o.kraf@mail.ru>
2025-09-08 08:44:32 +0200

    ubung1 update

--- com/cc/java/Uebung01.java
+++ com/cc/java/Uebung01.java
@@ -1,12 +1,19 @@
 package com.cc.java;
 
-public class Uebung01 
-{
+import java.util.Scanner;
 
+public class Uebung01 {
     public static void main(String[] args) {
         System.out.println("Uebung 1");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bitte gib eine Zahl ein: ");
+        int zahl = scanner.nextInt();
+        if (zahl % 2 == 0) {
+            System.out.println("Gerade");
+        } else {
+            System.out.println("Ungerade");
+        }
+        scanner.close();
     }
-    
 }
-
 
